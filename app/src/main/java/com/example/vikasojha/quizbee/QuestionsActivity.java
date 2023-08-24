f61dfde35b1a12138e4b560ff069747f8f01f8d7
Shiva Raj <sakilamshivaraj@gmail.com>
2023-08-24 16:00:56 +0530

    first commit

---
+++ app/src/main/java/com/example/vikasojha/quizbee/QuestionsActivity.java
@@ -0,0 +1,134 @@
+package com.example.vikasojha.quizbee;
+
+import android.content.Intent;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.text.TextUtils;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.RadioButton;
+import android.widget.RadioGroup;
+import android.widget.RelativeLayout;
+import android.widget.TextView;
+import android.widget.Toast;
+
+public class QuestionsActivity extends AppCompatActivity {
+    TextView tv;
+    Button submitbutton, quitbutton;
+    RadioGroup radio_g;
+    RadioButton rb1,rb2,rb3,rb4;
+
+    String questions[] = {
+                            "In which area MECS is located?",
+                            "How many blocks are there in MECS?",
+                            "Matrusri Education Society is in which state?",
+                            "What is the recent highest package of MECS?",
+                            "TECH TYCOONS CLUB belongs to which branch of MECS?",
+                            "What is the NAME of TECH fest in MECS?",
+                            "Number of passout batches from IT Dept of MECS are...",
+                            "Which Dept in MECS is not a part of NBA Accreditation as of 2023?",
+                            "Which clubs or organizations are not part of MECS as of 2023?",
+                            "What is the NAME of literary fest in MECS?"
+                         };
+    String answers[] = {
+            "Saidabad", "3","Telangana","25LPA","IT","SADHYA","Only one","IT","Political Club","URVI"};
+    String opt[] = {
+                    "Saidabad","Nampally","Kothapet","Chandrayangutta",
+                    "2","3","1","4",
+                    "Karnataka","AP","Telangana","Kerala",
+                    "25LPA","43LPA","15LPA","7LPA",
+                    "CME","CSE","CIVIL","IT",
+                    "URVI","SADHYA","LAXMI","SRIHITHI",
+                    "two","three","Only one","None",
+                     "ECE","CSE","CIVIL","IT",
+                    "Music Club","WITHYOU Org","Political Club","MCC",
+                     "SADHYA","SANKETHI","URVI","URVASHI"
+                   };
+    int flag=0;
+    public static int marks=0,correct=0,wrong=0;
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_questions);
+
+        final TextView score = (TextView)findViewById(R.id.textView4);
+        TextView textView=(TextView)findViewById(R.id.DispName);
+        Intent intent = getIntent();
+        String name= intent.getStringExtra("myname");
+
+        if (name.trim().equals(""))
+            textView.setText("Hello User");
+        else
+        textView.setText("Hello " + name);
+
+        submitbutton=(Button)findViewById(R.id.button3);
+        quitbutton=(Button)findViewById(R.id.buttonquit);
+        tv=(TextView) findViewById(R.id.tvque);
+
+        radio_g=(RadioGroup)findViewById(R.id.answersgrp);
+        rb1=(RadioButton)findViewById(R.id.radioButton);
+        rb2=(RadioButton)findViewById(R.id.radioButton2);
+        rb3=(RadioButton)findViewById(R.id.radioButton3);
+        rb4=(RadioButton)findViewById(R.id.radioButton4);
+        tv.setText(questions[flag]);
+        rb1.setText(opt[0]);
+        rb2.setText(opt[1]);
+        rb3.setText(opt[2]);
+        rb4.setText(opt[3]);
+        submitbutton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                //int color = mBackgroundColor.getColor();
+                //mLayout.setBackgroundColor(color);
+
+                if(radio_g.getCheckedRadioButtonId()==-1)
+                {
+                    Toast.makeText(getApplicationContext(), "Please select one choice", Toast.LENGTH_SHORT).show();
+                    return;
+                }
+                RadioButton uans = (RadioButton) findViewById(radio_g.getCheckedRadioButtonId());
+                String ansText = uans.getText().toString();
+//                Toast.makeText(getApplicationContext(), ansText, Toast.LENGTH_SHORT).show();
+                if(ansText.equals(answers[flag])) {
+                    correct++;
+                    Toast.makeText(getApplicationContext(), "Correct", Toast.LENGTH_SHORT).show();
+                }
+                else {
+                    wrong++;
+                    Toast.makeText(getApplicationContext(), "Wrong", Toast.LENGTH_SHORT).show();
+                }
+
+                flag++;
+
+                if (score != null)
+                    score.setText(""+correct);
+
+                if(flag<questions.length)
+                {
+                    tv.setText(questions[flag]);
+                    rb1.setText(opt[flag*4]);
+                    rb2.setText(opt[flag*4 +1]);
+                    rb3.setText(opt[flag*4 +2]);
+                    rb4.setText(opt[flag*4 +3]);
+                }
+                else
+                {
+                    marks=correct;
+                    Intent in = new Intent(getApplicationContext(),ResultActivity.class);
+                    startActivity(in);
+                }
+                radio_g.clearCheck();
+            }
+        });
+
+        quitbutton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intent=new Intent(getApplicationContext(),ResultActivity.class);
+                startActivity(intent);
+            }
+        });
+    }
+
+}
